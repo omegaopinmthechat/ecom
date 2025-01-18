b73938ac5dd929a1788ad72428f71b2d7e93628b
Amar <onlyaboutcoc@gmail.com>
2025-01-18 17:16:01 +0530

    initial commit

---
+++ app/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { assets } from '../assets/assets.js' //import the function assets
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between py-5 font-medium">
+
+        <div>Amar's Clothing</div>
+
+        <ul className="hidden">
+
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
